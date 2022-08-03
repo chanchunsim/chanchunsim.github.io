ec9562f20c26d2ff67a775695cbb8bc395245b67
Chun Sim <chanchunsim0918@e.newera.edu.my>
2022-08-03 21:01:44 +0800

    penang done

---
+++ groupassignment/JS/scripts.js
@@ -0,0 +1,28 @@
+window.addEventListener("scroll", function () {
+    var t = document.querySelector("#back-top");
+    t.classList.toggle("sticky", window.scrollY > 100);
+})
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    el_autohide = document.querySelector('.autohide');
+  
+    if(el_autohide){
+      var last_scroll_top = 0;
+      window.addEventListener('scroll', function() {
+            let scroll_top = window.scrollY;
+            let menu_collapse = document.querySelector('[aria-expanded]')?.getAttribute('aria-expanded')
+           if(scroll_top < last_scroll_top) {
+                el_autohide.classList.remove('scrolled-down');
+                el_autohide.classList.add('scrolled-up');
+            }
+            else if(scroll_top > 100){
+                if(menu_collapse == "false"){
+                  el_autohide.classList.remove('scrolled-up');
+                  el_autohide.classList.add('scrolled-down');
+                }
+            }
+            last_scroll_top = scroll_top + 1;
+      }); 
+    }
+});
